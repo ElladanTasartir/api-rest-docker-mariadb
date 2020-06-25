b06c9092f82a19ddcd79ad1f0d4692eebdf955f9
Erick Malta <erickmalta100@gmail.com>
2020-06-24 22:52:01 -0300

    Users CRUD finished

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -20,6 +20,62 @@ class UserController {
       return res.json(null);
     }
   }
+
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+      const user = await User.findByPk(id);
+      return res.json(user);
+    } catch (err) {
+      return res.json(null);
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) return res.status(400).json({ errors: ["Id não enviado"] });
+
+      const user = await User.findByPk(id);
+
+      if (!user)
+        return res.status(400).json({
+          erros: ["Usuário não existe"],
+        });
+
+      const newUser = await user.update(req.body);
+
+      return res.json(newUser);
+    } catch (err) {
+      return res
+        .status(400)
+        .json({ errors: err.errors.map((error) => error.message) });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) return res.status(400).json({ errors: ["Id não enviado"] });
+
+      const user = await User.findByPk(id);
+
+      if (!user)
+        return res.status(400).json({
+          erros: ["Usuário não existe"],
+        });
+
+      await user.destroy();
+
+      return res.json(user);
+    } catch (err) {
+      return res
+        .status(400)
+        .json({ errors: err.errors.map((error) => error.message) });
+    }
+  }
 }
 
 export default new UserController();
--- src/models/User.js
+++ src/models/User.js
@@ -52,7 +52,10 @@ export default class User extends Model {
     this.addHook("beforeSave", async (user) => {
       // Preenchemos o password_hash com os dados enviados no user.password
       // Segundo parâmetro é o salt
-      user.password_hash = await bcryptjs.hash(user.password, 8);
+
+      if (user.password) {
+        user.password_hash = await bcryptjs.hash(user.password, 8);
+      }
     });
 
     return this;
