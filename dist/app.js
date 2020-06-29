ee5bd153af0da6ff9c0b1f474c504813a429959e
ElladanTasartir <erickmalta100@gmail.com>
2020-06-29 14:15:41 -0300

    Added CORS and Helmet

--- dist/app.js
+++ dist/app.js
@@ -4,12 +4,28 @@ var _path = require('path');
 _dotenv2.default.config();
 
 var _express = require('express'); var _express2 = _interopRequireDefault(_express);
+var _cors = require('cors'); var _cors2 = _interopRequireDefault(_cors);
+var _helmet = require('helmet'); var _helmet2 = _interopRequireDefault(_helmet);
+
 var _home = require('./routes/home'); var _home2 = _interopRequireDefault(_home);
 var _user = require('./routes/user'); var _user2 = _interopRequireDefault(_user);
 var _token = require('./routes/token'); var _token2 = _interopRequireDefault(_token);
 var _aluno = require('./routes/aluno'); var _aluno2 = _interopRequireDefault(_aluno);
 var _photo = require('./routes/photo'); var _photo2 = _interopRequireDefault(_photo);
 require('./database'); // Vai ser executado automaticamente
+
+const whiteList = ["http://localhost:3000"];
+const corsOptions = {
+  origin: function (origin, callback) {
+    // Na origin vem o domnínio que estiver tentando acessar a API
+    // Se não for enviado, vem como undefined
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true); // recebe como primeiro parâmetro um erro
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
 
 // Estrutura de app com classes, só pra variar um pouco
 class App {
@@ -20,6 +36,8 @@ class App {
   }
 
   middlewares() {
+    this.app.use(_cors2.default.call(void 0, corsOptions));
+    this.app.use(_helmet2.default.call(void 0, ));
     this.app.use(_express2.default.urlencoded({ extended: true }));
     this.app.use(_express2.default.json());
     this.app.use(_express2.default.static(_path.resolve.call(void 0, __dirname, "uploads")));
--- src/app.js
+++ src/app.js
@@ -4,12 +4,28 @@ import { resolve } from "path";
 dotenv.config();
 
 import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+
 import homeRoutes from "./routes/home";
 import userRoutes from "./routes/user";
 import tokenRoutes from "./routes/token";
 import alunoRoutes from "./routes/aluno";
 import photoRoutes from "./routes/photo";
 import "./database"; // Vai ser executado automaticamente
+
+const whiteList = ["http://localhost:3000"];
+const corsOptions = {
+  origin: function (origin, callback) {
+    // Na origin vem o domnínio que estiver tentando acessar a API
+    // Se não for enviado, vem como undefined
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true); // recebe como primeiro parâmetro um erro
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
 
 // Estrutura de app com classes, só pra variar um pouco
 class App {
@@ -20,6 +36,8 @@ class App {
   }
 
   middlewares() {
+    this.app.use(cors(corsOptions));
+    this.app.use(helmet());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use(express.static(resolve(__dirname, "uploads")));
